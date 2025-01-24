01069f8cc04e8f2531a77e69772fca27eb37e011
vezendimate <vezendi.mate.vema77@gmail.com>
2025-01-24 12:59:37 +0100

    gjsrgjfgjnfg

--- 12C_2csoport_teszt-master/AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
+++ 12C_2csoport_teszt-master/AtvaltOOP_UnitTeszt/Atvalt_ClassUnitTest.cs
@@ -13,7 +13,7 @@ namespace AtvaltOOP_UnitTeszt
 
         Atvalt atvalt = new Atvalt();
         [TestMethod]
-        public void TestMethod1() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        public void isDecimal_True() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
         {
             // Arrange- Tesztkörnyezet beállítása
             string szoveg = "21";
@@ -21,8 +21,47 @@ namespace AtvaltOOP_UnitTeszt
             // Act- A vizsgált metódus végrehajtása
             kapottEredm = atvalt.isDecimal(szoveg);
             // Act- Az eredmény kiértékelése
-            Assert.AreEqual(kapottEredm, vartEredm); 
+            Assert.AreEqual(kapottEredm, vartEredm);
+
 
         }
+        [TestMethod]
+        public void isDecimal_False() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        {
+            // Arrange- Tesztkörnyezet beállítása
+            string szoveg = "k";
+            bool vartEredm = false, kapottEredm = false;
+            // Act- A vizsgált metódus végrehajtása
+            kapottEredm = atvalt.isDecimal(szoveg);
+            // Act- Az eredmény kiértékelése
+            Assert.AreEqual(kapottEredm, vartEredm);
+
+
+        }
+        [TestMethod]
+        public void isBinaris_True() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        {
+            string szoveg2 = "21";
+            bool vartEredm2 = true, kapottEredm2 = false;
+            // Act- A vizsgált metódus végrehajtása
+            kapottEredm2 = atvalt.isBinaris(szoveg2);
+            // Act- Az eredmény kiértékelése
+            Assert.AreEqual(kapottEredm2, vartEredm2);
+
+
+        }
+        [TestMethod]
+        public void isBinaris_False() // A tesztmetodus nevében jelzem, hoyg melyik metodust tesztelem és mire
+        {
+            string szoveg3 = "k";
+            bool vartEredm3 = true, kapottEredm3 = false;
+            // Act- A vizsgált metódus végrehajtása
+            kapottEredm3 = atvalt.isBinaris(szoveg3);
+            // Act- Az eredmény kiértékelése
+            Assert.AreEqual(kapottEredm3, vartEredm3);
+
+
+        }
+
     }
 }
